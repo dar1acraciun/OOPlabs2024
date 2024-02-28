0a86ca04ad108eb8f84612ab59042e7a4d1ac817
dar1acraciun <daria.craciun411@gmail.com>
2024-02-28 17:46:45 +0200

    problema 1

---
+++ lab1/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+long long convert(char stringg[200])
+{
+	long long  x=0;
+
+	for (int i = 0; i < strlen(stringg); i++)
+		if (stringg[i] >= '9'&& stringg[i] <= '9')
+			x = x * 10 + (stringg[i] - '0');
+	return x;
+}
+
+int main() {
+	FILE *fp;
+	long long s=0;
+	if (fopen_s(&fp, "in.txt", "r") != 0)
+		printf("EROARE\n");
+	else {
+		char stringg[201];
+		while (fgets(stringg, 201, fp))
+		{
+			stringg[strlen(stringg) - 1] = '\0';
+			long long  x = convert(stringg);
+			s += x;
+
+		}
+		printf("%d", s);
+	}
+
+
+}
